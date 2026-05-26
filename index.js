212d19f5f0fe0487f6fa784b2e99ff492572e05e
Rishit Singh <rishitsingh.286@gmail.com>
2026-05-26 20:42:14 +0530

    Refactor dotenv usage: replace dot-env with dotenv package and update configuration

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+import dotenv from "dotenv"
+
+dotenv.config({
+    path: "./.env",
+});
+
+let myUserName = process.env.username
+console.log(myUserName)
+
 console.log("Start of backend project")
